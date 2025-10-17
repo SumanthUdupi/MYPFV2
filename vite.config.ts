4c5f0d112e208ed55b724cdcd834cb644a203b46
Sumanth Udupi <109497622+SumanthUdupi@users.noreply.github.com>
2025-10-17 17:46:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Replace '<REPO_NAME>' with the name of your GitHub repository
-  // For example, if your repository is at https://github.com/your-username/my-portfolio,
-  // then the base should be '/my-portfolio/'
-  base: "/MYPFV2/",
   plugins: [react()],
+  base: '/MYPFV2/',
 })
